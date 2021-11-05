4c282e0cc25fb420bf6d47036f8ce939480eb1e1
Satyam Kumar Pathak <psatyam3006@gmail.com>
2021-11-05 19:24:23 +0530

    Added User Schema for database

---
+++ Server/Schema/user.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose')
+
+// For Hashing Password
+const bcrypt = require('bcryptjs')
+
+// Generating Token
+const jwt = require('jsonwebtoken')
+
+
+const userSchema = new mongoose.Schema({
+  Username: {
+    type: String,
+    required: true,
+    trim: true
+  }, 
+  age: {
+    type: Number,
+    default: 18,
+    validate(value) {
+      if (value <= 0) {
+        throw new Error("Age must be greater than 0")
+      }
+    }
+  },
+  phone: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  Certificates : [
+  {
+    filename : {
+      type: String,
+      default: null
+    },
+    status : {
+      type : Number,
+      default : 0
+    }, 
+    message : {
+      type : String,
+      default : null
+      }
+    }
+  ],
+  ProfileImage : {
+    type : String,
+    default : null
+  }
+})
+
+
+// Generating Auth token
+userSchema.methods.generateAuthToken = async function () { 
+  try {
+      const token = jwt.sign({_id : this._id}, process.env.SECRETKEY)
+      this.tokens = this.tokens.concat({ token : token })
+      await this.save()
+      return token
+  } 
+  catch (err) {
+    console.log(err)
+  }
+}
+
+// Hash The plain text password before saving into database
+userSchema.pre('save', async function (next) {
+  
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12)
+  }
+
+  next() 
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
